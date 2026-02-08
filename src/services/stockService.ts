9e062e17e0d35bcc2153b8b13d1702c0d7085e04
Muhammad Faisal <faisalmd25121999@gmail.com>
2026-02-08 17:52:06 +0530

    chanGES

--- src/services/stockService.ts
+++ src/services/stockService.ts
@@ -1,15 +1,9 @@
 import { Prisma, PrismaClient, Transaction } from "@prisma/client";
 import { prisma } from "../app";
 import { startOfDay, endOfDay } from "date-fns";
-import {
-  DefaultArgs,
-  PrismaClientOptions,
-} from "@prisma/client/runtime/library";
+import { DefaultArgs, PrismaClientOptions } from "@prisma/client/runtime/library";
 
-export const getDriverStock = async (
-  driverId: string,
-  date: Date = new Date(),
-) => {
+export const getDriverStock = async (driverId: string, date: Date = new Date()) => {
   const start = startOfDay(date);
   const end = endOfDay(date);
 
@@ -19,34 +13,16 @@ export const getDriverStock = async (
     },
   });
 
-  const todayBuy = transactions
-    .filter(
-      (t) =>
-        t.type === "BUY" ||
-        t.type === "SHOP_BUY" ||
-        (t.type === "PALTI" && t.paltiAction === "ADD"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todayBuy = transactions.filter((t) => t.type === "BUY" || t.type === "SHOP_BUY" || (t.type === "PALTI" && t.paltiAction === "ADD")).reduce((sum, t) => sum + Number(t.amount), 0);
 
-  const todaySell = transactions
-    .filter(
-      (t) =>
-        t.type === "SELL" ||
-        (t.type === "PALTI" && t.paltiAction === "SUBTRACT"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todaySell = transactions.filter((t) => t.type === "SELL" || (t.type === "PALTI" && t.paltiAction === "SUBTRACT")).reduce((sum, t) => sum + Number(t.amount), 0);
 
-  const todayWeightLoss = transactions
-    .filter((t) => t.type === "WEIGHT_LOSS")
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todayWeightLoss = transactions.filter((t) => t.type === "WEIGHT_LOSS").reduce((sum, t) => sum + Number(t.amount), 0);
 
   return todayBuy - todaySell - todayWeightLoss;
 };
 
-export const getDashboardStats = async (
-  driverId: string,
-  date: Date = new Date(),
-) => {
+export const getDashboardStats = async (driverId: string, date: Date = new Date()) => {
   const start = startOfDay(date);
   const end = endOfDay(date);
 
@@ -62,59 +38,26 @@ export const getDashboardStats = async (
     },
   });
 
-  const todayBuyKg = transactions
-    .filter(
-      (t) =>
-        t.type === "BUY" ||
-        t.type === "SHOP_BUY" ||
-        (t.type === "PALTI" && t.paltiAction === "ADD"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todayBuyKg = transactions.filter((t) => t.type === "BUY" || t.type === "SHOP_BUY" || (t.type === "PALTI" && t.paltiAction === "ADD")).reduce((sum, t) => sum + Number(t.amount), 0);
 
-  const todaySellKg = transactions
-    .filter(
-      (t) =>
-        t.type === "SELL" ||
-        (t.type === "PALTI" && t.paltiAction === "SUBTRACT"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todaySellKg = transactions.filter((t) => t.type === "SELL" || (t.type === "PALTI" && t.paltiAction === "SUBTRACT")).reduce((sum, t) => sum + Number(t.amount), 0);
 
-  const todayWeightLoss = transactions
-    .filter((t) => t.type === "WEIGHT_LOSS")
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todayWeightLoss = transactions.filter((t) => t.type === "WEIGHT_LOSS").reduce((sum, t) => sum + Number(t.amount), 0);
 
-  const todayFuelLiters = transactions
-    .filter((t) => t.type === "FUEL")
-    .reduce((sum, t) => sum + Number(t.amount), 0);
+  const todayFuelLiters = transactions.filter((t) => t.type === "FUEL").reduce((sum, t) => sum + Number(t.amount), 0);
 
   const todayStock = todayBuyKg - todaySellKg - todayWeightLoss;
 
-  const totalStockIn = allTransactions
-    .filter(
-      (t) =>
-        t.type === "BUY" ||
-        t.type === "SHOP_BUY" ||
-        (t.type === "PALTI" && t.paltiAction === "ADD"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount || 0), 0);
-  const totalStockOut = allTransactions
-    .filter(
-      (t) =>
-        t.type === "SELL" ||
-        (t.type === "PALTI" && t.paltiAction === "SUBTRACT"),
-    )
-    .reduce((sum, t) => sum + Number(t.amount || 0), 0);
-  const totalWeightLoss = allTransactions
-    .filter((t) => t.type === "WEIGHT_LOSS")
-    .reduce((sum, t) => sum + Number(t.amount || 0), 0);
+  const totalStockIn = allTransactions.filter((t) => t.type === "BUY" || t.type === "SHOP_BUY" || (t.type === "PALTI" && t.paltiAction === "ADD")).reduce((sum, t) => sum + Number(t.amount || 0), 0);
+  const totalStockOut = allTransactions.filter((t) => t.type === "SELL" || (t.type === "PALTI" && t.paltiAction === "SUBTRACT")).reduce((sum, t) => sum + Number(t.amount || 0), 0);
+  const totalWeightLoss = allTransactions.filter((t) => t.type === "WEIGHT_LOSS").reduce((sum, t) => sum + Number(t.amount || 0), 0);
 
   const totalStock = totalStockIn - totalStockOut - totalWeightLoss;
 
   return {
-    todayBuyKg,
-    todaySellKg,
-    todayFuelLiters,
-    todayStock: totalStock,
+    todayBuyKg: Number(todayBuyKg?.toFixed(2)),
+    todaySellKg: Number(todaySellKg?.toFixed(2)),
+    todayFuelLiters: Number(todayFuelLiters?.toFixed(2)),
+    todayStock: Number(totalStock?.toFixed(2)),
   };
 };
-
